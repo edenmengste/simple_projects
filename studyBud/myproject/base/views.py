3c9317d485229ea1be2f498aa36a2ab812c1bca6
Eden Mengste <edenmengste@gmail.com>
2025-08-04 21:05:21 +0000

    update

--- studyBud/myproject/base/views.py
+++ studyBud/myproject/base/views.py
@@ -8,7 +8,7 @@ rooms = [
 ]
 
 def home(request):
-    return render(request, "home.html", {"rooms": rooms})
+    return render(request, "base/home.html", {"rooms": rooms})
 
-def room(request):
-    return render(request , "room.html")
+def room(request, pk):
+    return render(request , "base/room.html")
