85beee8de73ed120f46f9d521b0e8fb6a4070f3d
edenmengste <edenmengste@gamil.com>
2025-08-08 01:50:11 +0400

    update the tables

--- studyBud/myproject/base/models.py
+++ studyBud/myproject/base/models.py
@@ -24,7 +24,7 @@ class Room(models.Model):
 class Message(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     room = models.ForeignKey(Room, on_delete=models.CASCADE) 
-    body = models.TextField
+    body = models.TextField(max_length=200, blank = True)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
