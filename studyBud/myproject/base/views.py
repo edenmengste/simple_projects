6012745ad8325cfa50f78a6ce376eccdb4e86fbc
edenmengste <edenmengste@gamil.com>
2025-08-08 01:09:31 +0400

    new tables added

--- studyBud/myproject/base/views.py
+++ studyBud/myproject/base/views.py
@@ -1,19 +1,16 @@
 from django.shortcuts import render
 from .models import Room
 
-rooms = [
-    { "id": 1, "name": "Lets learn Python!"},
-    { "id": 2, "name": "Design with me"},
-    { "id": 3, "name": "Frontend Developers"},
-]
+# rooms = [
+#     { "id": 1, "name": "Lets learn Python!"},
+#     { "id": 2, "name": "Design with me"},
+#     { "id": 3, "name": "Frontend Developers"},
+# ]
 
 def home(request):
     rooms = Room.objects.all()
     return render(request, "base/home.html", {"rooms": rooms})
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i["id"] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     return render(request , "base/room.html", {"room": room})
