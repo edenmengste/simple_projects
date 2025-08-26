97170b83f3dc7e5cf79e34cb79d4c7f4aa5672e5
edenmengste <edenmengste@gamil.com>
2025-08-26 23:01:43 +0400

    adding participants list in a room

--- studyBud/myproject/base/views.py
+++ studyBud/myproject/base/views.py
@@ -71,8 +71,17 @@ def home(request):
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    room_messages = room.message_set.all()
-    return render(request , "base/room.html", {"room": room, "room_messages": room_messages})
+    room_messages = room.message_set.all().order_by("-created")
+    participants = room.participants.all()
+
+    if request.method == "POST":
+        message = Message.objects.create(
+            user = request.user,
+            room = room,
+            body = request.POST.get("body")
+        )
+        return redirect("room", pk = room.id)
+    return render(request , "base/room.html", {"room": room, "room_messages": room_messages, "participants": participants})
 
 @login_required(login_url="login")
 def createRoom(request):
