3701abe90598ddc6376feb2b6bbf70297bc3efb3
edenmengste <edenmengste@gmail.com>
2025-08-27 16:27:25 +0400

    setting up user profile section

--- studyBud/myproject/base/views.py
+++ studyBud/myproject/base/views.py
@@ -67,7 +67,7 @@ def home(request):
         )
     topics = Topic.objects.all()
     room_count = rooms.count()
-    room_messages = Message.objects.all()
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
 
     return render(request, "base/home.html", {"rooms": rooms, "topics": topics, "room_count": room_count, "room_messages": room_messages})
 
@@ -85,6 +85,10 @@ def room(request, pk):
         room.participants.add(request.user)
         return redirect("room", pk = room.id)
     return render(request , "base/room.html", {"room": room, "room_messages": room_messages, "participants": participants})
+
+def userProfile(request):
+    context = {}
+    return render(request, "base/profile.html", context)
 
 @login_required(login_url="login")
 def createRoom(request):
