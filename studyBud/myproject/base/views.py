70bcf09d18f38fddad4bae94b7d2f2ad0a6fd831
edenmengste <edenmengste@gmail.com>
2025-09-01 23:10:06 +0400

    finished the profile editing

--- studyBud/myproject/base/views.py
+++ studyBud/myproject/base/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth import authenticate, login, logout
 from .models import Room, Topic, Message
 from django.contrib.auth.forms import UserCreationForm
 from django.db.models import Q
-from .forms import RoomForm
+from .forms import RoomForm, UserForm
 
 # rooms = [
 #     { "id": 1, "name": "Lets learn Python!"},
@@ -166,6 +166,14 @@ def deleteMessage(request, pk):
 
 @login_required(login_url="login")
 def updateUser(request):
-    context= {}
-    return render(request, "base/update-user.html", context)
+    user = request.user
+    form = UserForm(instance = user)
+
+    if request.method == "POST":
+        form = UserForm(request.POST, instance = user)
+        if form.is_valid():
+            form.save()
+            return redirect("profile", pk = user.id)
+
+    return render(request, "base/update-user.html", {"form":form})
 
