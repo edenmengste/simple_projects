894b2da4bf8c93b185aafd9a5ffb171bdca98505
edenmengste <edenmengste@gmail.com>
2025-09-02 17:01:12 +0400

    topics for mobile works

--- studyBud/myproject/base/views.py
+++ studyBud/myproject/base/views.py
@@ -65,7 +65,7 @@ def home(request):
         Q(name__icontains=q) |
         Q(description__icontains=q) 
         )
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[0:5]
     room_count = rooms.count()
     room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
 
@@ -177,3 +177,7 @@ def updateUser(request):
 
     return render(request, "base/update-user.html", {"form":form})
 
+def topicsPage(request):
+    q = request.GET.get("q") if request.GET.get("q") != None else ""
+    topics = Topic.objects.filter(name__icontains=q)
+    return render(request, "base/topics.html", {"topics": topics})
